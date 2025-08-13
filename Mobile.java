6e670e5d51d5320d6898315e332aefd7a91e0d7d
Jeevan555-cmd <jeevanpoojary555@gmail.com>
2025-08-13 15:01:08 +0530

    java program

---
+++ Mobile.java
@@ -0,0 +1,34 @@
+class Mobile{
+public static void main(String[]args){
+
+byte ram=12;
+ram=16;
+short storage=256;
+storage=512;
+int camera=200;
+long price=12500000;
+float size=6.5888f;
+char model='s';
+double resolution=12563.485662;
+boolean isAvailable=true;
+System.out.println("Mobile ram is:"+ram);
+
+System.out.println("Mobile storage is:"+storage);
+
+
+System.out.println("Mobile camera is:"+camera);
+
+System.out.println("Mobile Price is:"+price);
+
+System.out.println("MObile Size is"+size);
+System.out.println("Mobile model is:"+model);
+
+
+System.out.println("Mobile resolution is:"+resolution);
+
+System.out.println("Mobile is available:"+isAvailable);
+
+
+
+}
+}
