ccfa1fe182bd31a3b7740307d5250ebfd3c3d2d9
Jeevan555-cmd <jeevanpoojary555@gmail.com>
2025-08-14 14:57:52 +0530

    new array program

---
+++ Computer.java
@@ -0,0 +1,16 @@
+class Computer{
+public static void main (String[] args){
+String[] parts={"CPU","MOnitor","Speaker","Mouse","Keyboard","Printer","Motherboard","SSD","RAM","SSD","HDD"};
+System.out.println("The Parts of the Computer are:"+parts[0]);
+System.out.println( "2."+parts[1]);
+System.out.println("3."+parts[2]);
+System.out.println("4."+parts[3]);
+System.out.println("4."+parts[4]);
+System.out.println("5."+parts[5]);
+System.out.println("6."+parts[6]);
+System.out.println("7."+parts[7]);
+System.out.println("8."+parts[8]);
+System.out.println("9."+parts[9]);
+}
+
+}
---
+++ Mobiles.java
@@ -0,0 +1,16 @@
+class Mobiles{
+public static void main (String[] args){
+String[] type={"MI","VIVO","Nokia","Samsung","Motorola","Poco","IQ00"."Oneplus","Iphone","Asus"};
+System.out.println("Types of Mobiles are"+type[0]);
+System.out.println( "2."+type[1]);
+System.out.println("3."+type[2]);
+System.out.println("4."+type[3]);
+System.out.println("4."+type[4]);
+System.out.println("5."+type[5]);
+System.out.println("6."+type[6]);
+System.out.println("7."+type[7]);
+System.out.println("8."+type[8]);
+System.out.println("9."+type[9]);
+}
+
+}
