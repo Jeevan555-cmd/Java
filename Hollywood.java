5cda351e44ebe370cc2da3ca7c80ae8e0a02bdba
Jeevan555-cmd <jeevanpoojary555@gmail.com>
2025-08-18 14:54:54 +0530

    for loop program

---
+++ Hollywood.java
@@ -0,0 +1,15 @@
+class Hollywood{
+public static void main (String [] args){
+String movies[]={"Avatar","Ironman","Thor","Batman","Superman","Endgame","Inception","Hulk","Captain America","Infinity war"};
+
+for(int i=movies.length-1;i>=0;i--){
+
+System.out.println("The Indain states are:"+movies[i]);
+}
+
+for(String picture:movies){
+	System.out.println("The Movies are:"+picture);
+	
+}
+}
+}
