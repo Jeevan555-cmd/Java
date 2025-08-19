9994f4b88a6055188fd314c3f6db15354db19101
Jeevan555-cmd <jeevanpoojary555@gmail.com>
2025-08-19 15:03:15 +0530

    java if else program

---
+++ Convo.java
@@ -0,0 +1,16 @@
+class Convo{
+public static void main (String[] args){
+String he="Hi how are You";
+String she="hi how are You";
+
+
+if(he.length()==she.length()){
+	
+	System.out.println("Yes they are Same");
+	
+}
+else{
+System.out.println("No they are noot same");
+}
+}
+}
---
+++ Player.java
@@ -0,0 +1,16 @@
+class Player{
+public static void main (String[] args){
+int n=52;
+n=65;
+byte n1=65;
+
+if(n==n1){
+	
+	System.out.println("Yes they are Same");
+	
+}
+else{
+System.out.println("No they are noot same");
+}
+}
+}
