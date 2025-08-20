d66a0f28894905abbdfbe6c79f5074f5f0c352d7
Jeevan555-cmd <jeevanpoojary555@gmail.com>
2025-08-20 14:45:05 +0530

    java program of looping

---
+++ Iphone.java
@@ -0,0 +1,24 @@
+class Iphone{
+public static void main(String args[]){
+
+String ram="16GB";
+String storage="16GB";
+int price=125000;
+
+if(price<200000){
+System.out.println("This mobile price is low");
+
+if(ram == storage){
+	System.out.println("Both are same");
+	
+}
+
+
+else{
+	System.out.println("This mobile price is High");
+	
+}
+
+}
+}
+}
---
+++ Shoe.java
@@ -0,0 +1,23 @@
+class Shoe{
+public static void main(String args[]){
+	
+int shoe1=2500;
+int shoe2=3600;
+int shoe3=5324;
+
+if(shoe1<3000 && shoe2<4000){
+
+System.out.println("Shoe1 and shoe2 is Ok Pack it");
+
+}
+
+else if(shoe2<3000 && shoe3<5000){
+System.out.println("Shoe2 and Shoe3 is Ok Pack it");
+
+}
+else{
+	System.out.println("They are Costly");
+
+}
+}
+}
