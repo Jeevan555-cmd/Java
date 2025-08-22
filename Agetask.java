b37e3cd95c15b873e14c92a61359ef7ae56f739e
Jeevan555-cmd <jeevanpoojary555@gmail.com>
2025-08-22 14:56:05 +0530

    the java program of Do while

---
+++ Agetask.java
@@ -0,0 +1,13 @@
+class Agetask{
+public static void main (String[]args){
+int age=17;
+if(age>18){
+System.out.println("The age is above 18");
+}
+else{
+	System.out.println("The age should be above 18");
+	
+}
+
+}
+}
