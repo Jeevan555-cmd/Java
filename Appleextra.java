6bda0b8c5d77054b859c5ef4f867a46252838fb1
Jeevan555-cmd <jeevanpoojary555@gmail.com>
2025-09-02 00:05:34 +0530

    java static program

---
+++ Appleextra.java
@@ -0,0 +1,11 @@
+class Appleextra{
+static byte kilo=70;
+static short tons=500;
+static int price=7000500;
+static long phoneNumber=1236547895l;
+static boolean isAvailable=true;
+static char variety='G';
+static float gst=5.1526f;
+static double reviews=4.6231458;
+static String contents="The price of Apple may change on  Season";
+}
---
+++ Papaya.java
@@ -0,0 +1,17 @@
+class Papaya{
+
+public static void main (String[] args){
+	
+	
+	System.out.println(Papayaextra.kilo);
+	System.out.println(Papayaextra.tons);
+	System.out.println(Papayaextra.price);
+	System.out.println(Papayaextra.phoneNumber);
+	System.out.println(Papayaextra.isAvailable);
+	System.out.println(Papayaextra.variety);
+	System.out.println(Papayaextra.gst);
+	System.out.println(Papayaextra.reviews);
+	System.out.println(Papayaextra.contents);
+	
+	
+}}
