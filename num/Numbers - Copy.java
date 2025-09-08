23b245d3dc42567360f385c046124dceeb19f130
Jeevan555-cmd <jeevanpoojary555@gmail.com>
2025-09-08 14:37:15 +0530

    update

---
+++ num/Numbers - Copy.java
@@ -0,0 +1,16 @@
+class Numbers{
+public static void main(String []args){
+
+String Number[]={"One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten"};
+
+int i=0;
+while(i<=Number.length-1){
+System.out.println("The output:"+Number[i]);
+
+i++;
+}
+
+
+
+}
+}
