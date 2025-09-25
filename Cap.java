4811d8b8dd9abef8cc23f5155a9bcfd4d3ad6ef3
Jeevan555-cmd <jeevanpoojary555@gmail.com>
2025-09-25 15:13:40 +0530

    method overloading program

---
+++ Cap.java
@@ -0,0 +1,18 @@
+class Cap{
+public static void men(String company){
+System.out.println(company);
+
+}
+public static void men(int price,char type){
+System.out.println(price);
+System.out.println(type);
+
+}
+public static int men(double products){
+
+return 500;
+}
+public static void main(String[] args){
+men(100,'D');
+}
+}
