6fbf8165433dd4bb0f3c42410f1259faa0a705b9
Jeevan555-cmd <jeevanpoojary555@gmail.com>
2025-10-29 11:16:44 +0530

    Inheritance  variable examples

---
+++ Steel.java
@@ -0,0 +1,10 @@
+class Steel{
+int price=1000;
+long meter=50l;
+public void cost(){
+System.out.println("The price of the steel is :"+price);
+}
+public void length(){
+System.out.println("The length of a Steel is :"+meter);
+}
+}
